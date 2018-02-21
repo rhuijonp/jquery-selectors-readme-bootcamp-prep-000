1dca573c3ced2a461f492f2a37c0463923b4f67a
Rafael Huijon <rhuijonp@gmail.com>
2018-02-21 22:09:02 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+   return $('p');
+}
+
+function lastImageSelector() {
+	return $('ul li:last-child');
+}
+
+function ninjaBabySelector() {
+	return $("#baby-ninja");
+}
+
+function divSelector() {
+	return $(".pics");
+}
+
+function firstListItem() {
+	return $('ul#pic-list:first-child');
+}
